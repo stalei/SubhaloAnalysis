e64a54d595709f6db50867327daa4e09e82acd77
Shahram Talei <stalei@crimson.ua.edu>
2021-01-20 18:10:21 -0600

    abundance typo fixed

--- distributions.py
+++ distributions.py
@@ -6,6 +6,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.legend_handler import HandlerLine2D
