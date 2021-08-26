ba5e43fdce4af6d29a745fab44151e3b409f8b12
Shahram Talei <stalei@crimson.ua.edu>
2021-08-25 21:22:57 -0500

    MW m31 added

--- distributions.py
+++ distributions.py
@@ -16,6 +16,17 @@ import argparse
 
 plt.rcParams["font.size"] =12
 
+h_title='m12b,z=0'
+#MWSat=np.array([132,108,66,42,47,209,218,160,105,36,28,116,30,251,183,44,117,24,76,22,380,147,120,53,132,79,78,151,28,214,100,145,50,254,233,154,178,215,83,114,46,182,30,92,26,69,84,23,35,86,62,30,58,25,48,55,97,32,76,91,38]) # Drlica-Wagner_2020
+MWSat=np.array([24,28,29,23,19,26,29,35,32,33,41,37,43,43,40,46,50,46,45,50,48,52,54,61,64,63,78,79,82,81,86,87,87,87,92,98,102,94,108,105,116,116,128,133,126,141,148,155,161,170,182,188,203]) #Putman_2021
+MWSat_sigma=np.array([2.9,3.7,1.2,11.4,2.7,5.6,3.4,3.3,5.6,3.4,3.4,4.6,10.5,4,10.7,4.3,1.9,8.6,27.6,4.6,9.5,9.1,9.2,4.9,7.9,7,6.6,5.4,2.7,2.9,5.7,5.1,11.7,3.6,3.3,4.6,2.3,5.4,5.4]) #Putman_2021
+M31Sat=np.array([184,175,181,130,139,144,66,187,108,128,55,73,102,39,126,109,140,161,27,90,115,46,77,135,134,182]) #Putman_2021
+#MWSat_m=np.array([19,0.91,6.3,0.94,11,56,2.6,138000,12,4.6,1.3,1.1,190,14,0.26,0.23,25,6500,11,3.9,9.5,0.27])#*10^6# M_sun
+MWSat_m=np.array([74338.6888757733,152855.119951337,24787.3167488654,160947062.324042,179749.916896755,437685.953403389,86048.4629480132,258382.654826433,2027944.9174357,494778.661951076,215121.157370033,679250.637124876,2000361.58391393,245640.075889657,1092460.97614843,1438622739.9121,156190.800527338,4163394.58626644,391602758.62069,1880244.51725872,17004643.6057989,8899386.42443484,10986140.739585,446804.410765959,13239888.4358726,6724862.30489582,6302510.24713887,2170565.03422497,3615347.38513096,109551.752027455,43819077.1672987,2613722.0620459,50438504.9828027,577561.228435556,698939.061389366,120591.598619996,813961.887834364,1058150.45418467,11742861.0460984,4356370.91861309,10631749.1028242]) #Putman_2021 calculation
+M31Sat_m=np.array([122462743.288989,1637600.50593105,33257712.3203104,4821095.889461,74198190.1960031,17360575.1453082,1029003.9565436,5471716.43137707,1496320.24410118,2673541.00325403,32526318.994349,19273771.7204746,45752990.31932,7638624.77810868,61011784.3039265,30787433.8275809,24132225.3714772,13646376.2387283,19380792.6353565,48189223.4697131,37483484.5985755,190992472.379195,5135817.58442046,433145333.818762,2968616.14441648,86753096.39298,336243115.244791,44124177.9592858,7941780.919495,2989197.80532057,919140.502147116,1018959.69662228])
+
+M31Sat_sigma=np.array([24,4,7.8,6.4,16,11.8,2.9,5.7,4.6,7.1,10.2,9.3,8.6,10.9,7.1,11.5,8.4,5.3,92,3,7.8,35,14.8,4.5,5.8,2.6])
+
 def PlotShpere(ax,R,xc,yc,zc):
 	u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
 	x = xc+(R/1000.)*np.cos(u)*np.sin(v)
@@ -64,7 +75,7 @@ if __name__ == "__main__":
     MDLim=args.M_DwarfLim
     V_min=args.V_min
     nbins=60
-    nbins2=60
+    nbins2=17
     ## Mass Sample
     MG=MgAll[NumGAll>NLim]
     MC=McAll[NumCAll>NLim]
@@ -95,7 +106,7 @@ if __name__ == "__main__":
     RvHaloC=RvC[(MC>MHaloLow) & (MC<MHaloHigh)]
     #print some info
     print("No of the main halos:")
-    print("Gadget:",len(MHaloG))
+    print("DMO:",len(MHaloG))
     print("CoSANG:",len(MHaloC))
     #let's find the most massive halo in the given range:
     MMG_index=np.argmax(MHaloG)
@@ -133,7 +144,7 @@ if __name__ == "__main__":
 	####################
     #print len(MDwarfG)
     #print("No of halos belove %.4g"%DHL,"is:")
-    #print("Gadget:",len(MDwarfG))
+    #print("DMO:",len(MDwarfG))
     #print("NoDisk:",len(MDwarfC0))
     #print("CoSANG:",len(MDwarfC))
     ###############
@@ -147,13 +158,14 @@ if __name__ == "__main__":
     ZHC=ZHaloC[MMC_index]
     RvHC=RvHaloC[MMC_index]
     print("Halo mass:%g"%MHaloG[MMG_index])
+    print("Halo Rv:%g"%RvHaloG[MMG_index])
     print("Halo ID:%g"%IDHaloC[MMC_index])
     print("Halo pos:%g,%g,%g"%(XHaloC,YHaloC,ZHaloC))
     #let's plot these
     rHDwarfsG=np.sqrt((XDwarfG-XHG)**2.0+(YDwarfG-YHG)**2.0+(ZDwarfG-ZHG)**2.0)
     rHDwarfsC=np.sqrt((XDwarfC-XHC)**2.0+(YDwarfC-YHC)**2.0+(ZDwarfC-ZHC)**2.0)
-    rGlim=0.7*RvHG
-    rClim=0.7*RvHC
+    rGlim=1.0*RvHG
+    rClim=1.0*RvHC
     xg1=XDwarfG[rHDwarfsG<rGlim]
     yg1=YDwarfG[rHDwarfsG<rGlim]
     zg1=ZDwarfG[rHDwarfsG<rGlim]
@@ -166,7 +178,7 @@ if __name__ == "__main__":
     rc1=rHDwarfsC[rHDwarfsC<rClim]
     #print some info
     print("No of the sub halos:")
-    print("Gadget:",len(mg1))
+    print("DMO:",len(mg1))
     print("CoSANG:",len(mc1))
     #now let's extract all info for all halos in the given range
     #
@@ -190,65 +202,92 @@ if __name__ == "__main__":
     fig = plt.figure(1)
     #fig.suptitle('CoSANG vs N-Body ')
     ax11 = fig.add_subplot(221)
-    ax11.set_xlabel('X')
-    ax11.set_ylabel('Y')
-    ax11.set_title('Dwarfs Distribution G')
+    ax11.set_xlabel('X (Mpc)')
+    ax11.set_ylabel('Y (Mpc)')
+    ax11.set_title('Subhalo Distribution G')
     #ax11.plot(XHG,YHG,'b+')
-    ax11.scatter(XHG,YHG,c='red', alpha=0.9, marker='+',s=50)#RvHG)
+    ax11.scatter(XHG,YHG,c='red', alpha=0.99, marker='o',s=60)#RvHG)
     ax11.scatter(xg1,yg1,c='black', alpha=0.7, marker='.',s=15)
     ax12 = fig.add_subplot(222)
-    ax12.set_xlabel('X')
-    ax12.set_ylabel('Y')
-    ax12.set_title('Dwarfs Distribution C')
-    ax12.scatter(XHC,YHC,c='red', alpha=0.9, marker='+',s=50)#RvHC)
+    ax12.set_xlabel('X (Mpc)')
+    ax12.set_ylabel('Y (Mpc)')
+    ax12.set_title('Subhalo Distribution C')
+    ax12.scatter(XHC,YHC,c='red', alpha=0.99, marker='o',s=60)#RvHC)
     ax12.scatter(xc1,yc1,c='black', alpha=0.7, marker='.',s=15)
     ax13 = fig.add_subplot(223)
-    ax13.set_xlabel('X')
-    ax13.set_ylabel('Z')
-    ax13.set_title('Dwarfs Distribution G')
-    ax13.scatter(XHG,ZHG,c='red', alpha=0.9, marker='+',s=50)#RvHG)
+    ax13.set_xlabel('X (Mpc)')
+    ax13.set_ylabel('Z (Mpc)')
+    ax13.set_title('Subhalo Distribution G')
+    ax13.scatter(XHG,ZHG,c='red', alpha=0.99, marker='o',s=60)#RvHG)
     ax13.scatter(xg1,zg1,c='black', alpha=0.7, marker='.',s=15)
     ax14 = fig.add_subplot(224)
-    ax14.set_xlabel('X')
-    ax14.set_ylabel('Z')
-    ax14.set_title('Dwarfs Distribution C')
-    ax14.scatter(XHC,ZHC,c='red', alpha=0.9, marker='+',s=50)#RvHC)
+    ax14.set_xlabel('X (Mpc)')
+    ax14.set_ylabel('Z (Mpc)')
+    ax14.set_title('Subhalo Distribution C')
+    ax14.scatter(XHC,ZHC,c='red', alpha=0.99, marker='o',s=60)#RvHC)
     ax14.scatter(xc1,zc1,c='black', alpha=0.7, marker='.',s=15)
     #histogram of aboundances
     fig2 = plt.figure(2,figsize=plt.figaspect(2))
     #fig2.suptitle('CoSANG vs N-Body ')
     ax21 = fig2.add_subplot(211)
-    ax21.set_xlabel('$Log (M_{halo})$')
+    ax21.set_xlabel('$Log (M_{halo})[M_{\odot}]$')
     ax21.set_ylabel('$N_M(>M)$')
-    ax21.set_title('Dwarf Halos Mass abundance')
+    ax21.set_title('Dwarf Halos Mass Abundance')
+    ax21.set_xlim(6.0,10.8)
     #ax1.plot(d1['star_age'],d1['center h1']) #plot of main data
-    ax21.hist(np.log10(mg1),linewidth=2, bins=nbins, log=True,cumulative=-1, histtype='step', alpha=0.9,color='blue',label='Gadget')
+    ax21.hist(np.log10(mg1),linewidth=2, bins=nbins, log=True,cumulative=-1, histtype='step', alpha=0.9,color='blue',label='DMO')
     ax21.hist(np.log10(mc1),linewidth=2,bins=nbins,log=True, cumulative=-1, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    ax21.hist(np.log10(MWSat_m),linewidth=2,linestyle='-.',bins=nbins2,log=True, cumulative=-1, histtype='step', alpha=0.9,color='black',label='MW')
+    ax21.hist(np.log10(M31Sat_m),linewidth=2,linestyle='-.',bins=nbins2,log=True, cumulative=-1, histtype='step', alpha=0.9,color='gray',label='M31')
     ax21.legend(loc=1)
     ax22 = fig2.add_subplot(212)
-    ax22.set_xlabel('$r_{halo}[kpc]$')
+    ax22.set_xlabel('$r_{halo}/R_v$')
     ax22.set_ylabel('$N_r(<d)$')
-    ax22.set_title('Dwarf Halos Distance abundance')
+    ax22.set_title('Dwarf Halos Distance Abundance')
+    ax22.set_xlim(0.06,0.98)
     #ax1.plot(d1['star_age'],d1['center h1']) #plot of main data
-    ax22.hist(rg1*1000,linewidth=2, bins=nbins, log=True,cumulative=True, histtype='step', alpha=0.9,color='blue',label='Gadget')
-    ax22.hist(rc1*1000,linewidth=2,bins=nbins,log=True,cumulative=True, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    ax22.hist(rg1/RvHG,linewidth=2, bins=nbins, log=True,cumulative=True, histtype='step', alpha=0.9,color='blue',label='DMO')
+    ax22.hist(rc1/RvHC,linewidth=2,bins=nbins,log=True,cumulative=True, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    MWSat2=MWSat/140
+    ax22.hist(MWSat2,linewidth=2,linestyle='-.',bins=nbins,log=True,cumulative=True, histtype='step', alpha=0.9,color='black',label='MW')
+    M31Sat2=M31Sat/140
+    ax22.hist(M31Sat2,linewidth=2,linestyle='-.',bins=nbins,log=True,cumulative=True, histtype='step', alpha=0.9,color='gray',label='M31')
     ax22.legend(loc=2)
     #fig3 = plt.figure(3)
     #ax3 = fig3.add_subplot(111)
     #ax3.set_xlabel('$r_{halo}[kpc]$')
     #ax3.set_ylabel('$N$')
     #ax3.set_title('Total Dwarf Halos Distance aboundance')
-    #ax3.hist(rG2*1000,linewidth=2, bins=nbins2, log=False, histtype='step', alpha=0.9,color='blue',label='Gadget')
+    #ax3.hist(rG2*1000,linewidth=2, bins=nbins2, log=False, histtype='step', alpha=0.9,color='blue',label='DMO')
     #ax3.hist(rC2*1000,linewidth=2,bins=nbins2,log=False, histtype='step', alpha=0.9,color='green',label='CoSANG')
     #ax3.legend(loc=2)
     #We are done with the most massive halo, now let's look at all massive halos
     fig3 = plt.figure(3,figsize=plt.figaspect(0.5))
-    ax31=fig3.add_subplot(121) #log log + cumulative top down
-    ax31.hist(np.log10(VmaxDwarfG),linewidth=2, bins=nbins, log=True,cumulative=False, histtype='step', alpha=0.9,color='blue',label='Gadget')
+    ax31=fig3.add_subplot(121) #log log + cumulative top down,
+    ax31.set_xlabel('$Log(V_{max})$')
+    ax31.set_ylabel('$N_{V_{max}}$')
+    ax31.title.set_text(h_title)
+    ax31.hist(np.log10(VmaxDwarfG),linewidth=2, bins=nbins, log=True,cumulative=False, histtype='step', alpha=0.9,color='blue',label='DMO')
     ax31.hist(np.log10(VmaxDwarfC),linewidth=2, bins=nbins, log=True,cumulative=False, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    ax31.hist(np.log10(MWSat_sigma),linewidth=2,linestyle='-.', bins=nbins2, log=True,cumulative=False, histtype='step', alpha=0.9,color='black',label='MW')
+    ax31.hist(np.log10(M31Sat_sigma),linewidth=2,linestyle='-.', bins=nbins2, log=True,cumulative=False, histtype='step', alpha=0.9,color='gray',label='M31')
+    ax31.set_xlim(0.06,1.9)
+    ax31.set_ylim(0.7,1500)
+    ax31.legend(loc=1)
     ax32=fig3.add_subplot(122)
-    ax32.hist(np.log10(VmaxDwarfG),linewidth=2, bins=nbins, log=True,cumulative=True, histtype='step', alpha=0.9,color='blue',label='Gadget')
-    ax32.hist(np.log10(VmaxDwarfC),linewidth=2, bins=nbins, log=True,cumulative=True, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    ax32.set_xlabel('$Log(V_{max})$')
+    ax32.set_ylabel('$N(>V_{max})$') # np.log10(VmaxDwarfG)
+    ax32.title.set_text(h_title)
+    ax32.set_xlim(0.06,1.9)
+    ax32.set_ylim(0.7,10000)
+    ax32.hist(np.log10(VmaxDwarfG),linewidth=2, bins=nbins, log=True,cumulative=-1, histtype='step', alpha=0.9,color='blue',label='DMO')
+    ax32.hist(np.log10(VmaxDwarfC),linewidth=2, bins=nbins, log=True,cumulative=-1, histtype='step', alpha=0.9,color='red',label='CoSANG')
+    ax32.hist(np.log10(MWSat_sigma),linewidth=2,linestyle='-.', bins=nbins2, log=True,cumulative=-1, histtype='step', alpha=0.9,color='black',label='MW')
+    ax32.hist(np.log10(M31Sat_sigma),linewidth=2,linestyle='-.', bins=nbins2, log=True,cumulative=-1, histtype='step', alpha=0.9,color='gray',label='M31')
+    Kelly_v=[0.653,1.54]
+    Kelly_N=[2000,1]
+    ax32.plot(Kelly_v,Kelly_N,linewidth=2,linestyle=':',label='Kelly+19',color='gray')
+    ax32.legend(loc=1)
     plt.show()
 
 
@@ -265,13 +304,13 @@ rMc=np.sqrt(XMassiveC[MMC_index]**2. + YMassiveC[MMC_index]**2. + ZMassiveC[MMC_
 
 
 print("We selected this massive halo:")
-print("Gadget=%.4g"%MMassiveG[MMG_index],"solar mass & coordinates:",XMassiveG[MMG_index],",",YMassiveG[MMG_index],",",ZMassiveG[MMG_index])
+print("DMO=%.4g"%MMassiveG[MMG_index],"solar mass & coordinates:",XMassiveG[MMG_index],",",YMassiveG[MMG_index],",",ZMassiveG[MMG_index])
 print("NoDisk=%.4g"%MMassiveC0[MMC0_index],"solar mass & coordinates:",XMassiveC0[MMC0_index],",",YMassiveC0[MMC0_index],",",ZMassiveC0[MMC0_index])
 print("CoSANG=%.4g"%MMassiveC[MMC_index],"solar mass & coordinates:",XMassiveC[MMC_index],",",YMassiveC[MMC_index],",",ZMassiveC[MMC_index])
 
 
 print("No of sattelites closer than",ProbeRadius*1000," kpc is:")
-print("Gadget:",len(rSampleG))
+print("DMO:",len(rSampleG))
 print("NoDisk:",len(rSampleC0))
 print("CoSANG:",len(rSampleC))
 
@@ -286,7 +325,7 @@ ax1.set_xlabel('$Log (M_{halo})$')
 ax1.set_ylabel('$N$')
 ax1.set_title('Dwarf Halos Mass aboundance')
 #ax1.plot(d1['star_age'],d1['center h1']) #plot of main data
-ax1.hist(np.log10(MDwarfG),linewidth=2, bins=nbins, log=False, histtype='step', alpha=0.9,color='blue',label='Gadget')
+ax1.hist(np.log10(MDwarfG),linewidth=2, bins=nbins, log=False, histtype='step', alpha=0.9,color='blue',label='DMO')
 ax1.hist(np.log10(MDwarfC0),linewidth=2,bins=nbins,log=False, histtype='step', alpha=0.9,color='black',label='NoDisk')
 ax1.hist(np.log10(MDwarfC),linewidth=2,bins=nbins,log=False, histtype='step', alpha=0.9,color='green',label='CoSANG')
 
@@ -300,7 +339,7 @@ ax2.set_xlabel('$Log (M_{halo})$')
 ax2.set_ylabel('$N$')
 ax2.set_title('Mass aboundance')
 colors=['green','black','blue']
-labels=['CoSANG','NoDisk','Gadget']
+labels=['CoSANG','NoDisk','DMO']
 
 ax2.hist([Mclog, Mc0log, Mglog], bins=nbins, log=True, color=colors,label=labels)
 ax2.legend()
@@ -315,7 +354,7 @@ ax3.set_title('Subhalo Distribution for ~%.1g halo'%MMassiveG[MMG_index])
 
 
 
-ng, binsg, patchesg= ax3.hist(np.log10(rSampleG),linewidth=2,bins=nbins,log=False,cumulative=True, histtype='step', alpha=0.8,color='blue',label='Gadget')
+ng, binsg, patchesg= ax3.hist(np.log10(rSampleG),linewidth=2,bins=nbins,log=False,cumulative=True, histtype='step', alpha=0.8,color='blue',label='DMO')
 nc0, binsc0, patchesc0= ax3.hist(np.log10(rSampleC0),linewidth=2,bins=nbins,log=False,cumulative=True,histtype='step', alpha=0.8,color='black',label='NoDisk')
 nc, binsc, patchesc= ax3.hist(np.log10(rSampleC),linewidth=2,bins=nbins,log=False, cumulative=True,histtype='step', alpha=0.8,color='green',label='CoSANG')
 #ax3.legend()
@@ -350,7 +389,7 @@ ax2_1.set_ylabel('$n$')
 ax2_1.set_title('Dwarf Halos Mass Distribution')
 sns.distplot(np.log10(MDwarfC), hist = False, kde = True,kde_kws = {'linewidth': 3},label = 'CoSANG')
 sns.distplot(np.log10(MDwarfC0), hist = False, kde = True,kde_kws = {'linewidth': 3},label = 'NoDisk')
-sns.distplot(np.log10(MDwarfG), hist = False, kde = True,kde_kws = {'linewidth': 3},label = 'Gadget')
+sns.distplot(np.log10(MDwarfG), hist = False, kde = True,kde_kws = {'linewidth': 3},label = 'DMO')
 #fig2.legend(prop={'size': 16}, title = 'Airline')
 
 ###########################
